b8bea925a260f979675dd808db05b8c33b9616ce
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2021-12-02 18:13:11 +0900

    Add files via upload

---
+++ flutter_intl_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'customlocalizations.dart';
+
+void main() {runApp(const MyApp());}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Intl Package Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: const [
+        CustomLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('es', ''),
+      ],
+      home: const MyHomePage(title: 'Flutter Intl Package Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(CustomLocalizations.of(context)!.title.toString()),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(CustomLocalizations.of(context)!.message.toString(),),
+          ],
+        ),
+      ),
+    );
+  }
+}
