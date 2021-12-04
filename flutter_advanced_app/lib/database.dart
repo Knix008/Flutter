24d9e590fdbcb21c020127c45b054f0d26c6f105
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2021-12-04 22:48:53 +0900

    Add files via upload

---
+++ flutter_advanced_app/lib/database.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'expense.dart';
+
+class SQLiteDbProvider {
+  SQLiteDbProvider._();
+  static final SQLiteDbProvider db = SQLiteDbProvider._();
+  static final Future<Database> _database = initDB();
+
+  Future<Database> get database async {
+    return _database;
+  }
+
+  static Future<Database> initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String tempPath =documentsDirectory.path;
+    String path = join(tempPath, "ExpenseDB2.db");
+    return await openDatabase(
+        path, version: 1,
+        onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+          await db.execute(
+              "CREATE TABLE Expense ("
+              "id INTEGER PRIMARY KEY,"
+              "amount REAL,"
+              "date TEXT,"
+              "category TEXT"
+              ")");
+          await db.execute(
+              "INSERT INTO Expense ('id', 'amount', 'date', 'category') values (?, ?, ?, ?)",
+              [1, 1000, '2019-04-01 10:00:00', "Food"]);
+        }
+    );
+  }
+
+  Future<List<Expense>> getAllExpenses() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db.query("Expense",
+        columns: Expense.columns, orderBy: "date DESC");
+    List<Expense> expenses = List.empty(growable: true);
+    for (var result in results) {
+      Expense expense = Expense.fromMap(result);
+      expenses.add(expense);
+    }
+    return expenses;
+  }
+
+  Future<Expense?> getExpenseById(int id) async {
+    final db = await database;
+    var result = await db.query("Expense", where: "id = ", whereArgs: [id]);
+    if (result.isNotEmpty) {
+      return Expense.fromMap(result.first);
+    }
+  }
+
+  Future<double> getTotalExpense() async {
+    final db = await database;
+    List<Map> list =
+        await db.rawQuery("Select SUM(amount) as amount from expense");
+    return list.isNotEmpty ? list[0]["amount"] : Null;
+  }
+
+  insert(Expense expense) async {
+    final db = await database;
+    var maxIdResult =
+        await db.rawQuery("SELECT MAX(id)+1 as last_inserted_id FROM Expense");
+    var id = maxIdResult.first["last_inserted_id"];
+    var result = await db.rawInsert(
+        "INSERT Into Expense (id, amount, date, category)"
+        " VALUES (?, ?, ?, ?)",
+        [id, expense.amount, expense.date.toString(), expense.category]);
+    return result;
+  }
+
+  update(Expense product) async {
+    final db = await database;
+    var result = await db.update("Expense", product.toMap(),
+        where: "id = ?", whereArgs: [product.id]);
+    return result;
+  }
+
+  delete(int id) async {
+    final db = await database;
+    db.delete("Expense", where: "id = ?", whereArgs: [id]);
+  }
+}
---
+++ flutter_advanced_app/lib/main.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'expenseListModel.dart';
+import 'expense.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final expenses = ExpenseListModel();
+  runApp(ScopedModel<ExpenseListModel>(
+    model: expenses,
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Expense calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ScopedModelDescendant<ExpenseListModel>(
+          builder: (context, child, expenses) {
+            return ListView.separated(
+              itemCount: expenses.items.length + 1,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return ListTile(
+                      title: Text(
+                    "Total expenses: " + expenses.totalExpense.toString(),
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ));
+                } else {
+                  index = index - 1;
+                  return Dismissible(
+                      key: Key(expenses.items[index].id.toString()),
+                      onDismissed: (direction) {
+                        expenses.delete(expenses.items[index]);
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Item with id, " +
+                                expenses.items[index].id.toString() +
+                                " is dismissed")));
+                      },
+                      child: ListTile(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => FormPage(
+                                          id: expenses.items[index].id,
+                                          expenses: expenses,
+                                        )));
+                          },
+                          leading: const Icon(Icons.monetization_on),
+                          trailing: const Icon(Icons.keyboard_arrow_right),
+                          title: Text(
+                            expenses.items[index].category +
+                                ": " +
+                                expenses.items[index].amount.toString() +
+                                " \nspent on " +
+                                expenses.items[index].formattedDate,
+                            style: const TextStyle(
+                                fontSize: 18, fontStyle: FontStyle.italic),
+                          )));
+                }
+              },
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+            );
+          },
+        ),
+        floatingActionButton: ScopedModelDescendant<ExpenseListModel>(
+            builder: (context, child, expenses) {
+          return FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          ScopedModelDescendant<ExpenseListModel>(
+                              builder: (context, child, expenses) {
+                            return FormPage(
+                              id: 0,
+                              expenses: expenses,
+                            );
+                          })));
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
+        }));
+  }
+}
+
+class FormPage extends StatefulWidget {
+  const FormPage({Key? key, required this.id, required this.expenses})
+      : super(key: key);
+  final int id;
+  final ExpenseListModel expenses;
+
+  @override
+  _FormPageState createState() {
+    return _FormPageState(id: id, expenses: expenses);
+  }
+}
+
+class _FormPageState extends State<FormPage> {
+  _FormPageState({required this.id, required this.expenses});
+  final int id;
+  final ExpenseListModel expenses;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final formKey = GlobalKey<FormState>();
+  double? _amount;
+  DateTime? _date;
+  String? _category;
+
+  void _submit() {
+    final form = formKey.currentState;
+    if (form!.validate()) {
+      form.save();
+      if (id == 0) {
+        expenses.add(Expense(0, _amount!, _date!, _category!));
+      } else {
+        expenses.update(Expense(id, _amount!, _date!, _category!));
+      }
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Enter expense details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Amount',
+                    labelStyle: TextStyle(fontSize: 18)),
+                validator: (val) {
+                  String pattern = r'^[1-9]\d*(\.\d+)?$';
+                  RegExp regex = RegExp(pattern);
+                  if (!regex.hasMatch(val!)) {
+                    return 'Enter a valid number';
+                  } else {
+                    return null;
+                  }
+                },
+                initialValue:
+                    id == 0 ? '' : expenses.byId(id)!.amount.toString(),
+                onSaved: (val) => _amount = double.parse(val!),
+              ),
+              TextFormField(
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today),
+                  hintText: 'Enter date',
+                  labelText: 'Date',
+                  labelStyle: TextStyle(fontSize: 18),
+                ),
+                validator: (val) {
+                  String pattern =
+                      r'^((?:19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$';
+                  RegExp regex = RegExp(pattern);
+                  if (!regex.hasMatch(val!)) {
+                    return 'Enter a valid date';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (val) => _date = DateTime.parse(val!),
+                initialValue: id == 0 ? '' : expenses.byId(id)!.formattedDate,
+                keyboardType: TextInputType.datetime,
+              ),
+              TextFormField(
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.category),
+                    labelText: 'Category',
+                    labelStyle: TextStyle(fontSize: 18)),
+                onSaved: (val) => _category = val!,
+                initialValue:
+                    id == 0 ? '' : expenses.byId(id)!.category.toString(),
+              ),
+              ElevatedButton(
+                onPressed: _submit,
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
